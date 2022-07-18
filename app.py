a949bc8f3582a2ded22aa9b0aa96efa20b5fe0dd
NinaNusb <nina.nusbaumer@gmail.com>
2022-07-18 16:20:39 +0200

    modif initiale

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+
 from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") #différents url possibles du site
 def accueil():
     return render_template("accueil.html")
 
 @app.route('/article/<nom>')
 def article(nom):
-    return render_template("article.html", nom=nom)
+    return render_template("article.html", titre=nom)
+    
+@app.route("/admin/") #à compléter
+def admin():
+    pass
+    return render_template("article.html") 
