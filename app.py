a2f49df1c0973da68e6bdd9ad8e7a23668bc625e
StevenDelval <asteven59220@free.fr>
2022-07-19 08:39:09 +0200

    Import pymongo

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-
 from flask import Flask, render_template, redirect, url_for, request, session
+from pymongo import MongoClient
+
+client = MongoClient("localhost:27017")
 
 app = Flask(__name__)
 
