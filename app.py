62a981ecf259c0746f82f75b18a55e6bf477a191
StevenDelval <asteven59220@free.fr>
2022-07-20 10:17:35 +0200

    Add connexion

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from re import U
 from flask import Flask, render_template, redirect, url_for, request, session
 
 from pymongo import MongoClient
@@ -10,6 +11,7 @@ client = MongoClient("localhost:27017")
 
 db=client.blog
 articles = db.articles
+utilisateur=db.utilisateur
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='Secret'
@@ -17,30 +19,50 @@ app.config['SECRET_KEY']='Secret'
 
 @app.route("/") #différents url possibles du site
 def accueil():
-    return render_template("accueil.html" , articles = articles.find().sort('date',-1) )
+    try:
+        utilisateur = session["user"]
+    except:
+        utilisateur = None
+    return render_template("accueil.html" ,login = utilisateur, articles = articles.find().sort('date',-1) )
 
 @app.route('/article/<titre>')
 def article(titre):
-    return render_template("article.html", titre=titre, article = articles.find_one({"titre" : titre}))
+    try:
+        utilisateur = session["user"]
+    except:
+        utilisateur = None
+    return render_template("article.html",login = utilisateur, titre=titre, article = articles.find_one({"titre" : titre}))
 
 @app.route('/liste_articles/')
 def liste_articles():
-    return render_template("liste_articles.html", articles=articles.find())
+    try:
+        utilisateur = session["user"]
+    except:
+        utilisateur = None
+    return render_template("liste_articles.html",login = utilisateur, articles=articles.find())
 
 @app.route('/connexion',methods=['GET','POST'])
 def connexion():
+    utilisateur = {"username":"steven","password":"1234"}
     form = Connexion()
     if form.validate_on_submit():
-        return "connecter"
+        if form.data["login"] == utilisateur["username"] and form.data["password"] == utilisateur["password"]:
+            session["user"] = form.data["login"]
+            return redirect(url_for("accueil"))
+      
     return render_template("connexion.html",form = form)
 
 @app.route('/inscription',methods=['GET','POST'])
 def inscription():
     form =Inscription()
     if form.validate_on_submit():
-        return redirect( url_for("accueil"))
+        return redirect( "accueil")
     return render_template("inscription.html",form = form)
-    
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect(url_for("accueil"))
+
 @app.route("/admin/") #à compléter
 def admin():
     pass 
